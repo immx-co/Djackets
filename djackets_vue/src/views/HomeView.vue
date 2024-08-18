ccece71da42cb03ae7b2f765605cc3ce5e3be9a9
Вячеслав Орлов <slava.orlov.2001@mail.ru>
2024-08-18 14:37:12 +0300

    added opportunity view the latest products

--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -1,14 +1,79 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Welcome to Djacket
+            </p>
+            <p class="subtitle">
+                The best jacket store online
+            </p>
+        </div>
+    </section>
+
+    <div class="columns is-multiline">
+        <div class="column is-12">
+            <h2 class="is-size-2 has-text-centered">Latest products</h2>
+        </div>
+    </div>
+
+    <div 
+        class="column is-2 div-inline-block" 
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+    >
+        <div class="box">
+            <figure class="image mb-4">
+                <img v-bind:src="product.get_image">
+            </figure>
+
+            <h3 class="is-size-4">{{product.name}}</h3>
+            <p class="is-size-6 has-text-gray">{{product.price}} rubles</p>    
+
+            View details
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Home',
+  data() {
+    return {
+        latestProducts: []
+    }
+  },
   components: {
     
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+        axios.get('/api/v1/latest-products/').then(response => {
+            this.latestProducts = response.data
+        }).catch(error => {
+            console.log(error)
+        })
+    }
   }
 }
 </script>
+
+<style scoped>
+    .image {
+        margin-top: -1.25rem;
+        margin-left: -1.25rem;
+        margin-right: -1.25rem
+    }
+    
+    .div-inline-block {
+        display: inline-block;
+        width: 200px;
+        height: 200px;
+    }
+</style>
