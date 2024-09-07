ac7e3fb843e5518c4d999fbb227b46f7a799c2c9
immx <immx@DESKTOP-R1VTDBF>
2024-09-07 17:20:05 +0300

    added loading bar to the home page

--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -53,12 +53,16 @@ export default {
     this.getLatestProducts()
   },
   methods: {
-    getLatestProducts() {
-        axios.get('/api/v1/latest-products/').then(response => {
+    async getLatestProducts() {
+        this.$store.commit('setIsLoading', true)
+
+        await axios.get('/api/v1/latest-products/').then(response => {
             this.latestProducts = response.data
         }).catch(error => {
             console.log(error)
         })
+
+        this.$store.commit('setIsLoading', false)
     }
   }
 }
