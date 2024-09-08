d9784d4287020cab1023b654a048bc86cfc23bc2
immx <immx@DESKTOP-R1VTDBF>
2024-09-09 00:05:32 +0300

    added forgotten category vue

---
+++ djackets_vue/src/views/Category.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page-category">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { toast } from 'bulma-toast'
+
+export default {
+    name: "Category",
+    data() {
+        return {
+            category: {
+                products: []
+            }
+        }
+    },
+    mounted() {
+        this.getCategory()
+    },
+    methods: {
+        async getCategory() {
+            const categorySlug = this.$route.params.category_slug
+        }
+    }
+}
+</script>
