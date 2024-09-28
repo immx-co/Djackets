c5b714458dd0c4c2fd8828358b27b8975d2a1c1a
Вячеслав Орлов <slava.orlov.2001@mail.ru>
2024-09-28 13:35:03 +0300

    change contents of cart

--- djackets_vue/src/components/CartItem.vue
+++ djackets_vue/src/components/CartItem.vue
@@ -2,9 +2,13 @@
     <tr>
         <td><router-link :to="item.product.get_absolute_url">{{item.product.name}}</router-link></td>
         <td>{{item.product.price}} руб.</td>
-        <td>{{item.quantity}}</td>
+        <td>
+            {{item.quantity}}
+            <a @click="decrementQuantity(item)"><button>-</button></a>
+            <a @click="incrementQuantity(item)"><button>+</button></a>
+        </td>
         <td>{{getItemTotal(item).toFixed(2)}}</td>
-        <td><button class="delete"></button></td>
+        <td><button class="delete" @click="removeFromCart(item)"></button></td>
     </tr>
 </template>
 
@@ -23,6 +27,25 @@ export default {
     methods: {
         getItemTotal(item) {
             return item.quantity * item.product.price
+        },
+        decrementQuantity(item) {
+            item.quantity -= 1
+
+            if (item.quantity === 0) {
+                this.$emit('removeFromCart', item)
+            }
+            this.updateCart()
+        },
+        incrementQuantity(item) {
+            item.quantity += 1
+            this.updateCart()
+        },
+        updateCart() {
+            localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
+        },
+        removeFromCart(item) {
+            this.$emit('removeFromCart', item)
+            this.updateCart()
         }
     }
 }
